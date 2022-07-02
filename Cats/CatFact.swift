19e5413be0761c7138f539c93117a8f7565641c8
natruz <88256962+natruz@users.noreply.github.com>
2022-07-02 11:27:05 +0800

    New CatFact swift file and Decodable CatFact struct

---
+++ Cats/CatFact.swift
@@ -0,0 +1,13 @@
+//
+//  CatFact.swift
+//  Cats
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 2/7/22.
+//
+
+import Foundation
+
+struct CatFact: Decodable {
+    var fact: String
+    var length: Int
+}

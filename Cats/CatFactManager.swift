5524852deae234c77b2ddc0d1856302832b68f08
natruz <88256962+natruz@users.noreply.github.com>
2022-07-02 11:30:49 +0800

    @Published variable and decoding cataracts

--- Cats/CatFactManager.swift
+++ Cats/CatFactManager.swift
@@ -9,13 +9,21 @@ import Foundation
 import SwiftUI
 
 class CatFactManager: ObservableObject {
+    
+    @Published var fact: CatFact?
+    
     func getCatFact() {
         let apiURL = URL(string: "https://catfact.ninja/fact")!
         let request = URLRequest(url: apiURL)
         
+        fact = nil
+        
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let data = data {
-                print(String(data: data, encoding: .utf8)!)
+                let decoder = JSONDecoder ()
+                DispatchQueue.main.async {
+                    self.fact = try? decoder.decode(CatFact.self, from: data)
+                }
             }
         }
         .resume()

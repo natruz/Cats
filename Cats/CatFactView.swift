5a020b6a5cb001750f165b1b68022969965b7a73
natruz <88256962+natruz@users.noreply.github.com>
2022-07-02 11:08:19 +0800

    Setting up SwiftUIView files CatFactView and CatImageView

---
+++ Cats/CatFactView.swift
@@ -0,0 +1,20 @@
+//
+//  CatFactView.swift
+//  Cats
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 2/7/22.
+//
+
+import SwiftUI
+
+struct CatFactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CatFactView_Previews: PreviewProvider {
+    static var previews: some View {
+        CatFactView()
+    }
+}
---
+++ Cats/CatImageView.swift
@@ -0,0 +1,20 @@
+//
+//  CatImageView.swift
+//  Cats
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 2/7/22.
+//
+
+import SwiftUI
+
+struct CatImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CatImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CatImageView()
+    }
+}

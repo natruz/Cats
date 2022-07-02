2c300f8c76e999adabb8e7818759df6f0fce5ee7
natruz <88256962+natruz@users.noreply.github.com>
2022-07-02 11:05:48 +0800

    Initial Commit

---
+++ Cats/CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

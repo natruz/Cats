25c287246cc95fcdb74dc07873af3c4f7be1ed1f
natruz <88256962+natruz@users.noreply.github.com>
2022-07-02 11:25:15 +0800

    Testing the API

--- Cats/CatFactView.swift
+++ Cats/CatFactView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct CatFactView: View {
+    
+    @ObservedObject var catFactManager = CatFactManager()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .onAppear {
+                catFactManager.getCatFact()
+            }
     }
 }
 

011460b307a1478eb7ae456f5cefe5fd8cb9515f
natruz <88256962+natruz@users.noreply.github.com>
2022-07-02 11:39:12 +0800

    Import SwiftUI to CatImageView and added ObservableObject named CatImageManager

---
+++ Cats/CatImageManager.swift
@@ -0,0 +1,13 @@
+//
+//  CatImageManager.swift
+//  Cats
+//
+//  Created by NATALIE CHEN HUI REGINA RUZSICSK stu on 2/7/22.
+//
+
+import Foundation
+import SwiftUI
+
+class CatImageManager: ObservableObject {
+    
+}

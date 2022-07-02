a29a009fd60060c208136c5ea0813d61509bba0b
natruz <88256962+natruz@users.noreply.github.com>
2022-07-02 11:50:38 +0800

    Testing the CatImageView API

--- Cats/CatImageView.swift
+++ Cats/CatImageView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct CatImageView: View {
+    
+    @ObservedObject var catImageManger = CatImageManager()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .onAppear {
+                catImageManger.getCatImage()
+            }
     }
 }
 

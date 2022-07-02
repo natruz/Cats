4cebaeadb772f7d497c9afb1ea389952cc15b6c4
natruz <88256962+natruz@users.noreply.github.com>
2022-07-02 11:54:07 +0800

    Decoding catImages

--- Cats/CatImageManager.swift
+++ Cats/CatImageManager.swift
@@ -9,13 +9,20 @@ import Foundation
 import SwiftUI
 
 class CatImageManager: ObservableObject {
+    
+    @Published var image: UIImage?
+    
     func getCatImage() {
         let apiURL = URL(string: "https://cataas.com/c")!
         let request = URLRequest(url: apiURL)
         
+        image = nil
+        
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let data = data {
-                print(data)
+                DispatchQueue.main.async {
+                    self.image = UIImage(data: data)
+                }
             }
         }
         .resume()
